cf65267906eb5985b14882f2a3d4e6851fce23dc
le1st77427 <ozavolo1@stu.vistula.edu.pl>
2026-04-15 19:32:24 +0200

    script.js

--- zad4_javascript/script.js
+++ zad4_javascript/script.js
@@ -1,43 +1,33 @@
-// Zmiana motywu
+// ZMIANA MOTYWU
 
 function changeTheme() {
 
     const body = document.body;
 
     if (body.classList.contains("green-theme")) {
-
         body.classList.remove("green-theme");
         body.classList.add("red-theme");
-
     }
 
     else {
-
         body.classList.remove("red-theme");
         body.classList.add("green-theme");
-
     }
-
 }
 
 
 
-// Ukrywanie sekcji Projekty
+// UKRYWANIE SEKCJI PROJEKTY
 
 function toggleSection() {
 
     const section = document.getElementById("projekty");
 
     if (section.style.display === "none") {
-
         section.style.display = "block";
-
     }
 
     else {
-
         section.style.display = "none";
-
     }
-
 }
